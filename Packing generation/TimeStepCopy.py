c74f8fecaa1641dba6f77b6ac4ddada5f1e2dd33
lukyve <lucas.venemans@gmail.com>
2024-11-29 15:43:56 +0100

    TimeStepCopy code added
    TimeStepCopy.py copies all objects within a certain section to the areas next to it, in a function. The idea of this function is to run it every time step. Right now it is only functional for a single object, with multiple it stop working properly.

---
+++ Packing generation/TimeStepCopy.py
@@ -0,0 +1,41 @@
+import bpy
+
+Size = 10
+
+
+def SectionBounds(size):
+    max = size / 2
+    min = -max
+    return max, min
+
+def CopySelection():
+    for i in range(4):
+        obj = bpy.context.active_object
+        bpy.ops.object.duplicate()
+        if i == 0:
+            obj.location[0] = obj.location[0] + Size
+        if i == 1:
+            obj.location[0] = obj.location[0] - Size
+        if i == 2:
+            obj.location[1] = obj.location[1] + Size
+        if i == 3:
+            obj.location[1] = obj.location[1] - Size
+        
+
+def TimeStep():
+    Max = SectionBounds(Size)[0]
+    Min = SectionBounds(Size)[1]
+    bpy.ops.object.select_all(action='SELECT')
+    print("All objects selected.")
+    for obj in bpy.context.selected_objects:
+        loc = obj.location
+        if loc[2] > 0:
+            print("Object has a positive Z-coordinate.")
+            if loc[0] < Max and loc[0] > Min:
+                print("Object is within bounds on the X-axis.")
+                if loc[1] < Max and loc[1] > Min:
+                    print("Object is within bounds on the Y-axis. \n Starting Copy.")
+                    CopySelection()
+print("Starting time step.")
+TimeStep()
+print("Time step done.")
