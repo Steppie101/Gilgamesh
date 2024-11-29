7936d96db1d04914666746e5335cf63dd612b805
lukyve <lucas.venemans@gmail.com>
2024-11-29 16:54:24 +0100

    Update TimeStepCopy.py
    Added a function to delete the old time step. this function works for a single object, but if there are multiple the loop runs infinitely and crashes the program. I suspect this is because it copies one of the objects an extra time and adds it to the things it has to loop through.

--- Packing generation/TimeStepCopy.py
+++ Packing generation/TimeStepCopy.py
@@ -21,23 +21,53 @@ def CopySelection():
         if i == 3:
             obj.location[1] = obj.location[1] - Size
         
+def DeleteOldStep(location,max,min,object):
+    if location[2] < 0:
+        print("Object has a negative Z-coordinate. Deleting object.")
+        object.select_set(True)
+        bpy.context.view_layer.objects.active = object
+        bpy.ops.object.delete()
+    elif location[0] > max or location[0] < min:
+        print("Object is out of bounds on the X-axis. Deleting object.")
+        object.select_set(True)
+        bpy.context.view_layer.objects.active = object
+        bpy.ops.object.delete()
+    elif location[1] > max or location[1] < min:
+        print("Object is out of bounds on the Y-axis. Deleting object.")
+        object.select_set(True)
+        bpy.context.view_layer.objects.active = object
+        bpy.ops.object.delete()
+    else:
+        print("Object is within bounds.")
 
 def TimeStep():
     Max = SectionBounds(Size)[0]
     Min = SectionBounds(Size)[1]
     bpy.ops.object.select_all(action='DESELECT')
-    #print("All objects selected.")
+    
+    #Deletes the old step
+    print("Starting deletion.")
     for obj in bpy.data.objects:
         loc = obj.location
-        if loc[2] > 0:
+        DeleteOldStep(loc,Max,Min,obj)
+    print("Finished deletion.")
+    
+    
+    #Check if the object is in the middle section, and copies it.
+    #This does not yet include a check for the floor, this will need an extra if statement.
+    bpy.ops.object.select_all(action='DESELECT')
+    for obj1 in bpy.data.objects:
+        loc1 = obj1.location
+        if loc1[2] > 0:
             print("Object has a positive Z-coordinate.")
-            if loc[0] < Max and loc[0] > Min:
+            if loc1[0] < Max and loc1[0] > Min:
                 print("Object is within bounds on the X-axis.")
-                if loc[1] < Max and loc[1] > Min:
-                    print("Object is within bounds on the Y-axis. \n Starting Copy.")
-                    obj.select_set(True)
-                    bpy.context.view_layer.objects.active = obj
+                if loc1[1] < Max and loc1[1] > Min:
+                    print("Object is within bounds on the Y-axis. \nStarting Copy.")
+                    obj1.select_set(True)
+                    bpy.context.view_layer.objects.active = obj1
                     CopySelection()
+
 print("Starting time step.")
 TimeStep()
 print("Time step done.")
