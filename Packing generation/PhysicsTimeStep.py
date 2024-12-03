0189ad018a3776d12481dd1750d2a32988a337e3
Steppie <holyblobvish@gmail.com>
2024-12-03 16:25:33 +0100

    Generator working with many time steps

--- Packing generation/PhysicsTimeStep.py
+++ Packing generation/PhysicsTimeStep.py
@@ -4,8 +4,7 @@ import mathutils as mutils
 
 rng = np.random.default_rng()
 Size = 10
-maxIterations = 1000
-
+maxIterations = 50
 
 def SectionBounds(size):
     max = size / 2
@@ -57,37 +56,38 @@ def CopySelection():
         
 def DeleteOldStep(location,max,min,object):
     if location.z < 0:
-        print("Object has a negative Z-coordinate. Deleting object.")
         object.select_set(True)
         bpy.context.view_layer.objects.active = object
         bpy.ops.object.delete()
         return
     
     if location.x > max or location.x < min:
-        print("Object is out of bounds on the X-axis. Deleting object.")
         object.select_set(True)
         bpy.context.view_layer.objects.active = object
         bpy.ops.object.delete()
         return
     
     if location.y > max or location.y < min:
-        print("Object is out of bounds on the Y-axis. Deleting object.")
         object.select_set(True)
         bpy.context.view_layer.objects.active = object
         bpy.ops.object.delete()
         return
-    
-    print("Object is within bounds.")
 
 def TimeStep(current_frame):
     min, max = SectionBounds(Size)
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.object.visual_transform_apply()
+            
+    #Deletes the old step
+    bpy.ops.object.select_pattern(pattern = 'Cube*', extend = False)
+    for obj in bpy.context.selected_objects:
+        bpy.ops.object.select_all(action='DESELECT')
+        loc = obj.matrix_world.translation
+        DeleteOldStep(loc,max,min,obj)
     
     #Check if the object is in the middle section, and copies it.
     #This does not yet include a check for the floor, this will need an extra if statement.
     bpy.ops.object.select_pattern(pattern = 'Cube*', extend = False)
-    print("All objects selected.")
     for obj in bpy.context.selected_objects:
         bpy.ops.object.select_all(action='DESELECT')
         loc = obj.location
@@ -96,19 +96,13 @@ def TimeStep(current_frame):
             print("Object", obj.name, "has a negative Z-coordinate. Continuing...")
             continue
         
-        print("Object", obj.name, " has a positive Z-coordinate.")
-        
         if loc.x > max or loc.x < min:
             print("Object", obj.name, "is out of bounds on the X-axis. Continuing...")
             continue
         
-        print("Object", obj.name, " is within bounds on the X-axis.")
-        
         if loc.y > max or loc.y < min:
             print("Object", obj.name, "is out of bounds on the Y-axis. Continuing...")
             continue
-        
-        print("Object", obj.name, " is within bounds on the Y-axis. \nStarting Copy.")
         
         obj.select_set(True)
         bpy.context.view_layer.objects.active = obj
@@ -116,37 +110,27 @@ def TimeStep(current_frame):
         obj.location = location
         CopySelection()
     
-        #Perform time step
-        bpy.context.scene.frame_set(frame = current_frame)
-        
-        #Deletes the old step
-        print("Starting deletion.")
-        bpy.ops.object.select_pattern(pattern = 'Cube*', extend = False)
-        for obj in bpy.context.selected_objects:
-            bpy.ops.object.select_all(action='DESELECT')
-            loc = obj.matrix_world.translation
-            DeleteOldStep(loc,max,min,obj)
-        print("Finished deletion.")
+    #Perform time step
+    bpy.context.scene.frame_set(frame = current_frame)
 
-'''
+
+
 def main():
+    '''
     min, max = SectionBounds(Size)
     for i in range(maxIterations):
-        TimeStep(2 * i)
-        if (i % 50):
+        TimeStep(i)
+        if (i % 50 == 0):
                 rand_loc = random_location(min, max)
                 rand_rot = random_rotation()
                 rand_scale = random_scale(1, 0)
                 generate_particle(rand_loc, rand_rot, rand_scale, "ACTIVE")
-        
-        
+        print(i)
+    '''
     
+    for i in range(4):
+        TimeStep(i)
+        
 
 if __name__ == "__main__":
     main()
-'''
-
-print("Starting time step.")
-for i in range(20):
-    TimeStep(i)
-print("Time step done.")
