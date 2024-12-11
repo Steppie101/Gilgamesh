a317f01c0aebb018a3bf71aa5b35b844d114602a
Steppie <holyblobvish@gmail.com>
2024-12-11 17:06:00 +0100

    Added partially working cutoff and example stl's

--- Packing generation/PhysicsTimeStep.py
+++ Packing generation/PhysicsTimeStep.py
@@ -2,7 +2,7 @@ import bpy
 import numpy as np
 import mathutils as mutils
 
-rng = np.random.default_rng(4)
+rng = np.random.default_rng()
 xSize = 5
 ySize = 5
 maxIterations = 100
@@ -30,12 +30,12 @@ def GenerateParticle(location, rotation = mutils.Euler((0,0,0)), scale = mutils.
     obj.matrix_world = mutils.Matrix.LocRotScale(location, rotation, scale)
     obj.name = name
     bpy.ops.rigidbody.object_add(type = type)
-    obj.rigid_body.collision_shape = 'BOX'
+    obj.rigid_body.collision_shape = 'CONVEX_HULL'
     #obj.rigid_body.collision_margin = -0.01
 
 def CopySelection():    
-    bpy.ops.object.select_pattern(pattern = 'Cube*', extend = False)
-    for obj in bpy.context.selected_objects:      
+    bpy.ops.object.select_pattern(pattern = 'Particle*', extend = False)
+    for obj in bpy.context.selected_objects:
 
         location, rotation, scale = obj.matrix_world.decompose()
         x, y, z = location
@@ -55,7 +55,7 @@ def DeleteObject(obj):
     bpy.ops.object.delete()
 
 def DeleteOldStep():
-    bpy.ops.object.select_pattern(pattern = 'Cube*', extend = False)
+    bpy.ops.object.select_pattern(pattern = 'Particle*', extend = False)
     for obj in bpy.context.selected_objects:    
           
         location, rotation, scale = obj.matrix_world.decompose()
@@ -94,6 +94,14 @@ def ReorganizePeriodicly():
     
     bpy.ops.outliner.orphans_purge()
 
+def Intersect(obj1, obj2):
+    bpy.ops.object.select_all(action='DESELECT')
+    obj1.select_set(True)
+    bpy.context.view_layer.objects.active = obj1
+    bpy.ops.object.modifier_add(type='BOOLEAN')
+    obj1.modifiers["Boolean"].operation = 'INTERSECT'
+    obj1.modifiers["Boolean"].object = obj2
+    bpy.ops.object.modifier_apply(modifier="Boolean")
 
 def main():
     bpy.ops.object.select_all(action = 'SELECT')
@@ -111,7 +119,7 @@ def main():
             rand_loc = RandomLocation(xSize, ySize)
             rand_rot = RandomRotation()
             rand_scale = RandomScale(1, 0)
-            GenerateParticle(rand_loc, rand_rot, rand_scale, "ACTIVE", "Cube.{:03d}".format(n) + ".__")
+            GenerateParticle(rand_loc, rand_rot, rand_scale, "ACTIVE", "Particle.{:03d}".format(n) + ".__")
             n += 1
         
         #print("\x1b[1;33;40m" + "|" + round(i / maxIterations * 100) * "=" + ">" + "\x1b[1;31;40m" + (100 - round(i / maxIterations * 100)) * "-" + "|" + "\x1b[0m" + str(i) + "/" + str(maxIterations))
@@ -125,11 +133,26 @@ def main():
         bpy.context.scene.frame_set(frame = i)
     
     ReorganizePeriodicly()
+    bpy.context.scene.frame_set(frame = 0)
     
     print("\x1b[1;32;40m" + "|" + 101 * "=" + "|" + "\x1b[0m")
 
     plane = bpy.data.objects["Plane"]
     DeleteObject(plane)
+    
+    bpy.ops.object.select_all(action = 'SELECT')
+    bpy.context.view_layer.objects.active = bpy.context.selected_objects[0]
+    bpy.ops.object.join()
+    bpy.ops.rigidbody.object_remove()
+    set = bpy.context.selected_objects[0]
+    set.name = "Set"
+    
+
+    bpy.ops.mesh.primitive_cube_add(size = 1, location = mutils.Vector((0, 0, 12.5)), scale = mutils.Vector((xSize, ySize, 25)))
+    cube = bpy.data.objects["Cube"]
+
+    Intersect(set, cube)
+    DeleteObject(cube)
 
 
 if __name__ == "__main__":
