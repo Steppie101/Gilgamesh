1b146d1f20a97ed44289ba74bf84b01bfcb53859
jtdenhertog <j.t.denhertog@student.tudelft.nl>
2024-12-16 10:51:15 +0100

    Typo

--- Mesh_checking/stl_pygalmesh_to_vtk.py
+++ Mesh_checking/stl_pygalmesh_to_vtk.py
@@ -5,7 +5,7 @@ filename = "SphereSE.stl"
 os.system("cp ../Stls_via_Sebastiaan_Tim/Spheres/" + filename + " .")
 
 mesh = pygalmesh.generate_volume_mesh_from_surface_mesh(
-    "sphere_packing.stl",
+    filename,
     min_facet_angle=25.0,
     max_radius_surface_delaunay_ball=0.15,
     max_facet_distance=0.008,
